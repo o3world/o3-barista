93a509d0d05d687e74af2ae183e8a40fc4b3b18a
Cole Kettler <cole@o3world.com>
2016-09-18 23:30:56 -0400

    Create gulp lint task, configure JSHint

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+
+const lintFiles = ['**/*.js', '!node_modules/**/*'];
+
+gulp.task('lint', () => (
+  gulp.src(lintFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+));
