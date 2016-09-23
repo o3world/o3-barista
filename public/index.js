f4fe0981a3ad414df5bb93c15eab0524d3b95b6c
Cole Kettler <cole@o3world.com>
2016-09-23 15:30:35 -0400

    Fix cosmetic lint errors for routes and public index

--- public/index.js
+++ public/index.js
@@ -3,9 +3,9 @@ var app = angular.module("app", []);
 app.controller('name', ['$scope', '$http', function($scope, $http){
 	$http.get('/data')
 	.success((data)=>{
-		$scope.array = data
+		$scope.array = data;
 	})
 	.error((err)=>{
 		console.log(err);
-	})
-}])
+	});
+}]);
