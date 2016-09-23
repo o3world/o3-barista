edb49631b7f33b9a83fe880bd2392c7dd7882313
Cole Kettler <cole@o3world.com>
2016-09-23 15:19:45 -0400

    Remove unused dependencies

--- app.js
+++ app.js
@@ -2,14 +2,12 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var Twitter = require('twitter');
 var watson = require('watson-developer-cloud');
 var Promise = require("bluebird");
-var Step = require("step")
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
