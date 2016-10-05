6a8ea0c6504e238be2c5cb7fbe7248c7491940f7
Cole Kettler <cole@o3world.com>
2016-10-05 15:35:54 -0400

    Use two space indent in JS files

--- public/index.js
+++ public/index.js
@@ -2,12 +2,14 @@
 
 const app = angular.module("app", []);
 
-app.controller('name', ['$scope', '$http', function($scope, $http){
-	$http.get('/data')
-	.success((data)=>{
-		$scope.array = data;
-	})
-	.error((err)=>{
-		console.log(err);
-	});
+app.controller('name', [
+'$scope', '$http',
+function($scope, $http) {
+  $http.get('/data')
+  .success((data) => {
+    $scope.array = data;
+  })
+  .error((err) => {
+    console.log(err);
+  });
 }]);
