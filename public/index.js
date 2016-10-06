0a4887c1542bdf993113afa4267f045113065338
Cole Kettler <cole@o3world.com>
2016-10-06 11:16:56 -0400

    Create roast model, determine roast preference

--- public/index.js
+++ public/index.js
@@ -5,15 +5,45 @@ angular.module('app', [
 ])
 
 .controller('name', [
-'$scope', '$http',
-function($scope, $http) {
+'$scope', '$http', 'roastModel',
+function($scope, $http, roastModel) {
   $scope.getData = function() {
     $http.put('/api/watson/' + $scope.twitter)
       .success((data) => {
         $scope.array = data;
+        $scope.preference = roastModel.determinePreference(data);
       })
       .error((err) => {
         console.log(err);
       });
+  };
+}])
+
+.factory('roastModel', [
+'R',
+(R) => {
+  function determinePreference(profile) {
+    const curiosityPercentage = getCuriosityPercentage(profile);
+    const libertyPercentage = getLibertyPercentage(profile);
+
+    if (curiosityPercentage >= .8 || libertyPercentage >= .8) {
+      return 'light';
+    } else if (curiosityPercentage >= .5 || libertyPercentage >= .5) {
+      return 'medium';
+    } else {
+      return 'dark';
+    }
+  }
+
+  const findCuriosity = R.find(R.propEq('id', 'Curiosity'));
+  const findLiberty = R.find(R.propEq('id', 'Liberty'));
+
+  const getPercentage = R.prop('percentage');
+
+  const getCuriosityPercentage = R.pipe(findCuriosity, getPercentage);
+  const getLibertyPercentage = R.pipe(findLiberty, getPercentage);
+
+  return {
+    determinePreference
   };
 }]);
