0c9ea1830c115ea9825806a8f184f47a41e156ba
Cole Kettler <cole@o3world.com>
2016-11-12 00:12:35 -0500

    Reject parent traits

--- public/personality.controller.js
+++ public/personality.controller.js
@@ -3,20 +3,25 @@
 angular.module('personality.controller', [])
 
 .controller('PersonalityController', [
-'$scope', '$http', 'personalityService',
-($scope, $http, personalityService) => {
+'$scope', '$http', 'R', 'personalityService',
+($scope, $http, R, personalityService) => {
   $scope.roastOptions = [
     'light',
     'medium',
     'dark'
   ];
 
+  const isParent = id => id.indexOf('_parent') !== -1;
+  const rejectParentTraits = R.reject(trait => isParent(trait.id));
+
   $scope.getData = () => {
     $http
       .put('/api/watson/' + $scope.twitter)
       .then(personalityResponse => {
         const profile = personalityResponse.data;
-        $scope.array = profile;
+
+        const profileWithoutParents = rejectParentTraits(profile);
+        $scope.array = profileWithoutParents;
 
         $scope.preference = personalityService.determinePreference(profile);
         $scope.curiosity = personalityService.getCuriosityPercentage(profile);
