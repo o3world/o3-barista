aa841ed317a6731e0b18c83303414d73d9b17b65
Cole Kettler <cole@o3world.com>
2016-11-12 00:45:25 -0500

    Use arrow functions in all routes

--- routes/watson.js
+++ routes/watson.js
@@ -23,15 +23,15 @@ const twitterClient = new Twitter({
   access_token_secret: process.env.O3_BARISTA_TWITTER_ACCESS_TOKEN_SECRET
 });
 
-router.put('/:twitteruser', function(req, res) {
+router.put('/:twitteruser', (req, res) => {
   const username = req.params.twitteruser;
 
   fetchTwitterFeed(username)
-    .then(function(result) {
+    .then(result => {
       personalityInsightsClient.profile({
         text: result,
         language: 'en'
-      }, function(err, response) {
+      }, (err, response) => {
         if (err) {
           console.log('error:', err);
         } else {
@@ -43,15 +43,15 @@ router.put('/:twitteruser', function(req, res) {
         }
       });
     })
-    .catch(function(e) {
+    .catch(e => {
       console.log(e);
     });
 });
 
 function fetchTwitterFeed(username) {
-  return new Promise(function(resolve) {
+  return new Promise(resolve => {
     const params = { screen_name: username, count: 5000 };
-    twitterClient.get('statuses/user_timeline', params, function(error, tweets) {
+    twitterClient.get('statuses/user_timeline', params, (error, tweets) => {
       if (error) {
         console.error(error);
       }
@@ -86,11 +86,11 @@ function parsePersonalityResponse(response) {
   const array = [];
   const i = response.tree.children;
 
-  i.forEach(function(thing) {
-    thing.children.forEach(function(thing2) {
+  i.forEach(thing => {
+    thing.children.forEach(thing2 => {
       const obj = { id: thing2.id, percentage: thing2.percentage };
       array.push(obj);
-      thing2.children.forEach(function(thing4) {
+      thing2.children.forEach(thing4 => {
         const obj = { id: thing4.id, percentage: thing4.percentage };
         array.push(obj);
       });
